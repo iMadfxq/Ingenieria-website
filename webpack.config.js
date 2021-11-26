9ceeec799e952c88fdedbfccceba9c5126c42945
iMadfxq <oscarfelipe123456789@gmail.com>
2021-11-25 19:21:23 -0600

    Integrated webpack to my project

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { mainModule } = require('process');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, 
+        'css-loader',
+        'sass-loader'
+      ],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/html/index.html',
+      filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin(),
+  ]
+};
